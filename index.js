0d2ccab1ee17dd97a3d36e7d33134d5a904679cd
PracovniTipy <dudypetr1@seznam.cz>
2026-06-24 20:55:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("PracovniTipyAI běží");
 });
@@ -11,6 +13,14 @@ app.get("/generate", (req, res) => {
 
 app.post("/generate", (req, res) => {
   res.send("OK");
+});
+
+app.post("/herohero", async (req, res) => {
+  console.log(req.body);
+
+  res.json({
+    success: true
+  });
 });
 
 app.listen(process.env.PORT || 3000);
