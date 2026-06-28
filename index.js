e73b85ad72cd89791ae389de586be667e0b7ae01
PracovniTipy <dudypetr1@seznam.cz>
2026-06-28 19:26:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const path = require("path");
 const { createCanvas, loadImage } = require("canvas");
 const { v2: cloudinary } = require("cloudinary");
 
+const ffmpeg = require("fluent-ffmpeg");
+const ffmpegPath = require("ffmpeg-static");
+
+ffmpeg.setFfmpegPath(ffmpegPath);
+
 const app = express();
 
 app.use(express.json());
@@ -14,6 +19,10 @@ cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
+    const ffmpeg = require("fluent-ffmpeg");
+const ffmpegPath = require("ffmpeg-static");
+
+ffmpeg.setFfmpegPath(ffmpegPath);
 });
 
 const PORT = process.env.PORT || 3000;
