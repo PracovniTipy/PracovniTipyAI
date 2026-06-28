815b224e84d2489a65e257be88e381bde98c05c9
PracovniTipy <dudypetr1@seznam.cz>
2026-06-28 20:43:33 +0200

    Configure ffmpeg for video processing
    Added ffmpeg configuration for video processing.

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ app.use(express.json());
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+    const ffmpeg = require("fluent-ffmpeg");
+const ffmpegPath = require("ffmpeg-static");
+
+ffmpeg.setFfmpegPath(ffmpegPath);
 });
 
 const PORT = process.env.PORT || 3000;
