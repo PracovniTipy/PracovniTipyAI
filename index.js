f84f05dd5822fe6cf13981458457a876fb307e4a
PracovniTipy <dudypetr1@seznam.cz>
2026-06-28 20:28:01 +0200

    Fix cloudinary config

--- index.js
+++ index.js
@@ -18,11 +18,7 @@ app.use(express.json());
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-    const ffmpeg = require("fluent-ffmpeg");
-const ffmpegPath = require("ffmpeg-static");
-
-ffmpeg.setFfmpegPath(ffmpegPath);
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const PORT = process.env.PORT || 3000;
