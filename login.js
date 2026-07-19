6b8ed1cf4a276d27cfa53d8f3a0b4c1b8769c889
PracovniTipy <dudypetr1@seznam.cz>
2026-07-19 15:54:50 +0200

    Add Playwright script for login page automation

--- login.js
+++ login.js
@@ -1,1 +1,12 @@
+const { chromium } = require("playwright");
 
+(async () => {
+  const browser = await chromium.launch({
+    headless: false
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto("https://herohero.co/login");
+
+})();
